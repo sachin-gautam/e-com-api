eccbc71a89bc939710b95466f8a69f7296d8672b
unknown <sachin-gautam>
2024-11-08 20:06:48 +0545

    Added graceful shutdown using context

--- cmd/crud-api/main.go
+++ cmd/crud-api/main.go
@@ -1,9 +1,15 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
+	"log/slog"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/sachin-gautam/go-crud-api/internal/config"
 )
@@ -25,8 +31,28 @@ func main() {
 	}
 
 	fmt.Printf("Sever Started %s", cfg.HttpServer.Address)
-	if err := server.ListenAndServe(); err != nil {
-		log.Fatal("Failed to start server")
+
+	done := make(chan os.Signal, 1)
+
+	signal.Notify(done, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+
+	go func() {
+		if err := server.ListenAndServe(); err != nil {
+			log.Fatal("Failed to start server")
+		}
+	}()
+
+	<-done
+
+	slog.Info("Shutting down the server")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	if err := server.Shutdown(ctx); err != nil {
+		slog.Error("Failed to shutdown server", slog.String("error", err.Error()))
 	}
+
+	slog.Info("Server shutdown sucessfully")
 
 }
