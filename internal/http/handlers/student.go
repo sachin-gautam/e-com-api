526b13dbe43d4b01c13512f2e5f104fb3c081a79
unknown <sachin-gautam>
2024-11-10 19:23:03 +0545

    added repsonse handler

--- internal/http/handlers/student.go
+++ internal/http/handlers/student.go
@@ -3,6 +3,7 @@ package student
 import (
 	"encoding/json"
 	"errors"
+	"fmt"
 	"io"
 	"log/slog"
 	"net/http"
@@ -14,14 +15,23 @@ import (
 func New() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 
+		slog.Info("Creating a Student")
+
 		var student dtypes.Sudent
 
-		if err := json.NewDecoder(r.Body).Decode(&student); errors.Is(err, io.EOF) {
-			response.WriteJson(w, http.StatusBadRequest, response.GeneralError(err))
+		err := json.NewDecoder(r.Body).Decode(&student)
+
+		if errors.Is(err, io.EOF) {
+			response.WriteJson(w, http.StatusBadRequest, response.GeneralError(fmt.Errorf("empty body")))
 			return
 		}
 
-		slog.Info("Creating a Student")
+		if err != nil {
+			response.WriteJson(w, http.StatusBadGateway, response.GeneralError(err))
+			return
+		}
+
+		//Request Validation
 
 		response.WriteJson(w, http.StatusCreated, map[string]string{"success": "OK"})
 	}
